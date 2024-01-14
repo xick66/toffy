843651113a07454ab1b7734b80886aeb2d6ea220
Building <126589920+xick66@users.noreply.github.com>
2024-01-14 16:55:46 +0530

    Update QuestionView.swift
    added the same old single question view instead of 5 questions

--- toffy/Views/QuestionView.swift
+++ toffy/Views/QuestionView.swift
@@ -4,33 +4,32 @@
 //
 //  Created by Rahul K M on 01/01/24.
 //
+
 import SwiftUI
 
 struct QuestionView: View {
     @EnvironmentObject var viewModel: QuestionViewModel
     @State private var userResponse: String = ""
-
+    
     var body: some View {
         VStack {
-            // Display questions if they are not completed
             if !viewModel.hasCompletedQuestions && viewModel.currentQuestionIndex < viewModel.questions.count {
                 let question = viewModel.questions[viewModel.currentQuestionIndex]
-
+                
                 Text(question)
                     .font(.title2)
                     .fontWeight(.bold)
                     .multilineTextAlignment(.center)
                     .padding()
-
+                
                 TextEditor(text: $userResponse)
                     .frame(height: 200)
                     .border(Color.gray, width: 1)
                     .padding()
-
+                
                 Button("Submit") {
                     viewModel.submitResponse(userResponse)
                     userResponse = ""
-                    // Check if all questions are answered to generate guidance
                     if viewModel.currentQuestionIndex == viewModel.questions.count {
                         viewModel.generatePersonalizedGuidance()
                     }
@@ -39,20 +38,70 @@ struct QuestionView: View {
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(10)
-            }
-
-            // Display the personalized guidance if questions are completed
-            else if viewModel.hasCompletedQuestions {
+            } else if viewModel.hasCompletedQuestions {
                 Text(viewModel.personalizedGuidance)
                     .padding()
             }
+        }
+        .onAppear {
+            viewModel.fetchQuestionsFromLLM()  // Fetch questions when the view appears
         }
     }
 }
 
 struct QuestionView_Previews: PreviewProvider {
-    static var previews: some View {
-        let viewModel = QuestionViewModel()
-        return QuestionView().environmentObject(viewModel)
-    }
+static var previews: some View {
+let viewModel = QuestionViewModel()
+return QuestionView().environmentObject(viewModel)
 }
+}
+
+
+//import SwiftUI
+//
+//struct QuestionView: View {
+//    @EnvironmentObject var viewModel: QuestionViewModel
+//    @State private var userResponse: String = ""
+//
+//    var body: some View {
+//        VStack {
+//            // Check if there are more questions to ask
+//            if !viewModel.hasCompletedQuestions && viewModel.currentQuestionIndex < viewModel.questions.count {
+//                let question = viewModel.questions[viewModel.currentQuestionIndex]
+//
+//                Text(question)
+//                    .font(.title2)
+//                    .fontWeight(.bold)
+//                    .multilineTextAlignment(.center)
+//                    .padding()
+//
+//                TextEditor(text: $userResponse)
+//                    .frame(height: 200)
+//                    .border(Color.gray, width: 1)
+//                    .padding()
+//
+//                Button("Submit") {
+//                    viewModel.submitResponse(userResponse)
+//                    userResponse = ""
+//                }
+//                .padding()
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .cornerRadius(10)
+//            }
+//            // If all questions have been answered, display the guidance
+//            else if viewModel.hasCompletedQuestions {
+//                Text(viewModel.personalizedGuidance)
+//                    .padding()
+//            }
+//        }
+//    }
+//}
+//
+//struct QuestionView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuestionView().environmentObject(QuestionViewModel())
+//    }
+//}
+
+
