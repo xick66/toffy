ee3496953871dd7552461e01577729be7b85f560
Building <126589920+xick66@users.noreply.github.com>
2024-03-17 17:25:07 +0530

    Update QuestionView.swift

--- toffy/Views/QuestionView.swift
+++ toffy/Views/QuestionView.swift
@@ -26,13 +26,13 @@ struct QuestionView: View {
                         .padding()
                 }
 
-                // Text editor for user response
+                
                 TextEditor(text: $userResponse)
                     .frame(height: 200)
                     .border(Color.gray, width: 1)
                     .padding()
 
-                // Submit button
+              
                 Button("Submit") {
                     viewModel.submitResponse(userResponse)
                     userResponse = ""
@@ -42,7 +42,7 @@ struct QuestionView: View {
                 .foregroundColor(.white)
                 .cornerRadius(10)
             } else {
-                // Display personalized guidance after all questions are answered
+            
                 Text(viewModel.personalizedGuidance)
                     .padding()
             }
@@ -60,52 +60,4 @@ struct QuestionView_Previews: PreviewProvider {
         return QuestionView().environmentObject(viewModel)
     }
 }
-
-//import SwiftUI
-//
-//struct QuestionView: View {
-//    @EnvironmentObject var viewModel: QuestionViewModel
-//    @State private var userResponse: String = ""
-//
-//    var body: some View {
-//        VStack {
-//            // Check if there are more questions to ask
-//            if !viewModel.hasCompletedQuestions && viewModel.currentQuestionIndex < viewModel.questions.count {
-//                let question = viewModel.questions[viewModel.currentQuestionIndex]
-//
-//                Text(question)
-//                    .font(.title2)
-//                    .fontWeight(.bold)
-//                    .multilineTextAlignment(.center)
-//                    .padding()
-//
-//                TextEditor(text: $userResponse)
-//                    .frame(height: 200)
-//                    .border(Color.gray, width: 1)
-//                    .padding()
-//
-//                Button("Submit") {
-//                    viewModel.submitResponse(userResponse)
-//                    userResponse = ""
-//                }
-//                .padding()
-//                .background(Color.blue)
-//                .foregroundColor(.white)
-//                .cornerRadius(10)
-//            }
-//            // If all questions have been answered, display the guidance
-//            else if viewModel.hasCompletedQuestions {
-//                Text(viewModel.personalizedGuidance)
-//                    .padding()
-//            }
-//        }
-//    }
-//}
-//
-//struct QuestionView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        QuestionView().environmentObject(QuestionViewModel())
-//    }
-//}
-
 
